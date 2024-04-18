704d032ab7bdf1a6dd241c63818efab133208f4a
Sudhir <sudhir.nayak.b@gmail.com>
2024-04-18 18:05:55 +0530

    Day 1 Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+/* Component Decorator */
 @Component({
+  /* HTML selector inside which the template will be rendered */
   selector: 'app-root',
+  //template file location
   templateUrl: './app.component.html',
+  //style file location
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'First Angular App';
+  counter = 10; 
+
+  click(e:any) { 
+    this.title = "New Title" + this.counter; 
+    this.counter+=10;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './page-header.component';
 
+//Decorator - NGModule 
 @NgModule({
+  /*declarations- this section lists all the components and directives used/defined in the 
+  application. It is a comma separated list of values. */
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent
   ],
+  /*imports - lists all the external modules that are required in this module. */
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  /* exports - lists all the app modules that will be made available to 
+  other external applications - optional */ 
+  /* providers - lists all the services and pipes defined within the application
+   and these services listed here will be injected by Angular where ever required.
+   */
   providers: [],
+  /* bootstrap - only the top-most Module in Angular can define this section. 
+   No other Module within the application can define it. 
+   - Lists the components that should be rendered/loaded when the application starts
+   */
   bootstrap: [AppComponent]
 })
 export class AppModule { }
